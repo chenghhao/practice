4d0c9f9f581fa856aa02e8206a69d71701e45999
your name <your email>
2020-09-22 10:20:20 +0800

    第一次commit

---
+++ src/day1.java
@@ -0,0 +1,18 @@
+class day1 {
+    public int lastWordLength(String s) {
+       int lastWordLength=0;
+       int lenth=s.length();
+       for(int i=0;i<lenth;i++){
+           if(true){
+
+           }
+
+
+       }
+
+        return 0;
+    }
+public static void main(){
+
+}
+}
